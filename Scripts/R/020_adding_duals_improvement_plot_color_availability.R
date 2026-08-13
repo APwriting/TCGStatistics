704af3c6137ef6ff77f562add8c6e4aa40094897
FalkN <falknagies@proton.me>
2026-08-13 09:45:14 +0200

    Created new script 021 for plotting the Monte Carlo Simulation of starting hands with dual-lands.

--- Scripts/R/020_adding_duals_improvement_plot_color_availability.R
+++ Scripts/R/020_adding_duals_improvement_plot_color_availability.R
@@ -32,9 +32,34 @@ dual_lands_3color.df <- read.table(
 
 
 # Create dual_lands by adding columns 1_2 and 1_3
-dual_lands_3color.df$dual_lands_combined <- dual_lands_3color.df$X1_2 + dual_lands_3color.df$X1_3
+dual_lands_3color.df$dual_lands_combined <- dual_lands_3color.df$X1_2 + dual_lands_3color.df$X1_3 +  dual_lands_3color.df$X2_3
 
 
+#Stats
+
+average_change_2_color <- mean(diff(dual_lands_2color.df$X2.1  ))
+average_change_2_color;
+
+
+average_change_2_color_first_10 <- mean(diff(dual_lands_2color.df$X2.1[1:10]  ))
+average_change_2_color_first_10;
+
+diffs_2_color = diff(dual_lands_2color.df$X2.1  );
+diffs_2_color_perc = diffs_2_color[-1] / diffs_2_color[-length(diffs_2_color)];
+diffs_2_color_perc;
+
+dual_lands_3color.df
+
+average_change_3_color <- mean(diff(dual_lands_3color.df$X3.1  ))
+average_change_3_color;
+
+
+average_change_3_color_first_10 <- mean(diff(dual_lands_3color.df$X3.1[1:10]  ))
+average_change_3_color_first_10;
+
+diffs_3_color = diff(dual_lands_3color.df$X3.1  );
+diffs_3_color_perc = diffs_3_color[-1] / diffs_3_color[-length(diffs_3_color)];
+diffs_3_color_perc;
 
 #Plotting
 
@@ -43,14 +68,27 @@ dual_lands_3color.df$dual_lands_combined <- dual_lands_3color.df$X1_2 + dual_lan
 
 
 dual2_plot = ggplot(dual_lands_2color.df, aes(x = X1_2, y =  X2.1)) +
-  geom_point() +
+  geom_line()+
+  geom_point(color="brown2") +
   labs(
-    x = "Dual lands",
-    y = "Probability to have all colors in starting hand"
-  )
+    x = "Dual lands replacing basics (40 lands)",
+    y = "Probability to have all 2 colors\nin starting hand"
+  )+
+  #ggtitle("2 color deck improvement by adding dual lands:")+
+  background_grid(major = "y", minor = "y");
 dual2_plot;
 
 
+dual3_plot = ggplot(dual_lands_3color.df, aes(x = dual_lands_combined, y =  X3.1)) +
+  geom_line()+
+  geom_point(color="brown2") +
+  labs(
+    x = "Dual lands replacing basics (40 lands)",
+    y = "Probability to have all 3 colors\nin starting hand"
+  )+
+  #ggtitle("2 color deck improvement by adding dual lands:")+
+  background_grid(major = "y", minor = "y");
+dual3_plot;
 
 
 
@@ -59,10 +97,17 @@ dual2_plot;
 
 
 
+###
+#Saving the plots
 
 
+plot_name = paste("Chapter_7_3_1__dual_land_increase_2_color__", Sys.Date()  ,".png")
+ggsave(plot_name, plot = dual2_plot, 
+       width = 6, height = 4, dpi = 300);
 
-
+plot_name = paste("Chapter_7_3_2__dual_land_increase_3_color__", Sys.Date()  ,".png")
+ggsave(plot_name, plot = dual3_plot, 
+       width = 6, height = 4, dpi = 300);
 
 
 
---
+++ Scripts/R/021_analysing_duals_improvement_color_availability__MC.R
@@ -0,0 +1,143 @@
+#/usr/bin/r
+
+#Loading Libraries
+
+library(ggplot2);
+library(cowplot);
+theme_set(theme_cowplot());
+library(ggpubr);
+library(geomtextpath);
+
+library(dplyr)
+
+#Own Libraries
+library(AnaMTG);
+
+#Setting path
+current_directoy = getwd();
+setwd("../../")
+
+
+# Read the data
+data.df <- read.table(
+  "Scripts/Python/020_analysis_draws_dual_lands_MC/020_Saved_run_for_plotting__2_colors_test.txt",
+  header = TRUE,
+  sep = "\t"
+)
+
+data.df = data.df[which(data.df$Color_Counts!="Mulligan"),];
+
+
+digested_2_color_duals.df <- data.df %>%
+  group_by(Dual_land) %>%
+  mutate(
+    dual_sum = sum(Color_Counts_during_trials)
+  ) %>%
+  slice_max(
+    order_by = Color_Counts_during_trials,
+    n = 1,
+    with_ties = FALSE
+  ) %>%
+  mutate(
+    proportion = Color_Counts_during_trials / dual_sum
+  ) %>%
+  ungroup()
+
+
+# Read the data
+data.df <- read.table(
+  "Scripts/Python/020_analysis_draws_dual_lands_MC/020_Saved_run_for_plotting__3_colors.txt",
+  header = TRUE,
+  sep = "\t"
+)
+
+data.df = data.df[which(data.df$Color_Counts!="Mulligan"),];
+
+
+digested_3_color_duals.df <- data.df %>%
+  group_by(Dual_land) %>%
+  mutate(
+    dual_sum = sum(Color_Counts_during_trials)
+  ) %>%
+  slice_max(
+    order_by = Color_Counts,
+    n = 1,
+    with_ties = FALSE
+  ) %>%
+  mutate(
+    proportion = Color_Counts_during_trials / dual_sum
+  ) %>%
+  ungroup()
+digested_3_color_duals.df;
+
+# Read the data
+data.df <- read.table(
+  "Scripts/Python/020_analysis_draws_dual_lands_MC/020_Saved_run_for_plotting__4_colors.txt",
+  header = TRUE,
+  sep = "\t"
+)
+
+data.df = data.df[which(data.df$Color_Counts!="Mulligan"),];
+
+
+digested_4_color_duals.df <- data.df %>%
+  group_by(Dual_land) %>%
+  mutate(
+    dual_sum = sum(Color_Counts_during_trials)
+  ) %>%
+  slice_max(
+    order_by = Color_Counts,
+    n = 1,
+    with_ties = FALSE
+  ) %>%
+  mutate(
+    proportion = Color_Counts_during_trials / dual_sum
+  ) %>%
+  ungroup()
+digested_4_color_duals.df;
+
+
+# Read the data
+data.df <- read.table(
+  "Scripts/Python/020_analysis_draws_dual_lands_MC/020_Saved_run_for_plotting__5_colors.txt",
+  header = TRUE,
+  sep = "\t"
+)
+
+data.df = data.df[which(data.df$Color_Counts!="Mulligan"),];
+
+
+digested_5_color_duals.df <- data.df %>%
+  group_by(Dual_land) %>%
+  mutate(
+    dual_sum = sum(Color_Counts_during_trials)
+  ) %>%
+  slice_max(
+    order_by = Color_Counts,
+    n = 1,
+    with_ties = FALSE
+  ) %>%
+  mutate(
+    proportion = Color_Counts_during_trials / dual_sum
+  ) %>%
+  ungroup()
+digested_5_color_duals.df;
+
+
+#Analysis
+
+#4 color
+average_change_4_color <- mean(diff(digested_4_color_duals.df$proportion  ))
+average_change_4_color;
+
+
+average_change_4_color_first_10 <- mean(diff(digested_4_color_duals.df$proportion[1:10]  ))
+average_change_4_color_first_10;
+
+diffs_4_color = diff(digested_4_color_duals.df$proportion  );
+diffs_4_color_perc = diffs_4_color[-1] / diffs_4_color[-length(diffs_4_color)];
+diffs_4_color_perc;
+
+
+
+

c6bccbec74ddab366742508203250733e8b55705
APWriting <falknagies@proton.me>
2026-08-31 18:21:57 +0200

    Adjusted script to use YAML file

--- Scripts/Python/030_2_031__kubernetes_cluster_initiation_SQL_database/030_init_Kubernetes_cluster.py
+++ Scripts/Python/030_2_031__kubernetes_cluster_initiation_SQL_database/030_init_Kubernetes_cluster.py
@@ -3,20 +3,37 @@
 #script to initiate a Kubernetes Cluster to store a postgreSQL database for TCG_data
 
 import subprocess
+import yaml
+
+# Read cluster configuration
+with open("clusters.yaml", "r") as file:
+    config = yaml.safe_load(file)
 
 cluster_name = "TCG_data_cluster"
-
+# Get existing Kubernetes clusters
 result = subprocess.run(
     ["kind", "get", "clusters"],
     capture_output=True,
     text=True
 )
 
-if cluster_name not in result.stdout.splitlines():
-    subprocess.run(
-        ["kind", "create", "cluster", "--name", cluster_name],
-        check=True
-    )
-    print("Cluster created.")
-else:
-    print("Cluster already exists.")
+existing_clusters = result.stdout.splitlines()
+
+
+# Create clusters from YAML data
+for cluster in config["clusters"]:
+
+    cluster_name = cluster["name"]
+
+    if cluster_name not in existing_clusters:
+
+        subprocess.run(
+            ["kind", "create", "cluster", "--name", cluster_name],
+            check=True
+        )
+
+        print(f"Cluster '{cluster_name}' created.")
+
+    else:
+
+        print(f"Cluster '{cluster_name}' already exists.")
